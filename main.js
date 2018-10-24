c2bd0036f457ef0dc38db12083d84baba542d2c3
gregology <greg@gho.st>
2018-10-24 18:53:26 -0400

    bootstraped drop and drag zip files

--- main.js
+++ main.js
@@ -1,36 +1,21 @@
 const { app, BrowserWindow, Menu } = require('electron')
+const sqlite3 = require('sqlite3')
 var path = require('path')
 
-  let mainWindow;
-  let menuTemplate = [
-      {
-          label: "Window Manager",
-          submenu: [
-              { label: "create New" }
-          ]
-      },
-      {
-        label : "View",
-              submenu : [
-          { role : "reload" },
-          { label : "custom reload" }
-          ]
-      }
-  ];
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+  })
 
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      icon: path.join(__dirname, 'assets/icons/png/64x64.png')
-    })
+  // and load the index.html of the app.
+  win.loadFile('app/index.html')
 
-    // and load the index.html of the app.
-    win.loadFile('index.html')
+  win.webContents.openDevTools()
 
-    let menu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(menu);
-  }
+  Menu.setApplicationMenu();
+}
 
-  app.on('ready', createWindow)
+app.on('ready', createWindow)
